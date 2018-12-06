62fd4465dc9eaab49b06a96a7c2d08f196ff3ff3
AVINASH BANSI PATOLE <45585479+ap27051@users.noreply.github.com>
2018-12-06 19:51:06 +0530

    Sample code for if else loop
    This code explains how if else loop works.

---
+++ if_else.py
@@ -0,0 +1,18 @@
+#flow control learning code
+
+print("Welcome to Patole's Python Academy")
+
+print("This code will help you understand how to you use if else in python")
+
+print("Kindly Enter The User Name")
+name = input()
+
+if name == "Avinash":
+
+	print("You are a valid user")
+
+else:
+
+	print("You are not a valid user")
+
+
