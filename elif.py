8671cfee2d4e56ecdfa4425a70a506a2007d49fd
AVINASH BANSI PATOLE <45585479+ap27051@users.noreply.github.com>
2018-12-06 21:08:56 +0530

    Create elif.py
    I am getting an error while running this code, I am unsure where I am going wrong

---
+++ elif.py
@@ -0,0 +1,26 @@
+#flow control learning code
+#This code is an example of elif statement
+
+print("Welcome to Patole's Python Academy " + '\n')
+
+print("This code will help you understand how to you use elif in python" + '\n')
+
+print("What is your name")
+name = input()
+
+print("what is your age")
+age = input()
+
+if name == "Avinash":
+
+	print("Welcome Avinash!")
+
+elif age < 18:
+
+	print("You are a young child")
+	
+elif age > 18:
+	
+    print("You are an adult")
+
+
